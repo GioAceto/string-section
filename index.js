c9ac26b860c5ca3434dcb939658b1ce182b22abc
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-03-27 22:47:32 -0400

    fix function returns the correct camel case

--- index.js
+++ index.js
@@ -9,30 +9,28 @@ function stringIncludes(haystack, needle) {
 }
 
 function countLetter(haystack, needle) {
-  let countArray = []
   let count = 0
 
   for (let i = 0; i < haystack.length; i++) {
-    countArray.push(haystack[i])
-    if (countArray[i] === needle) {
-      count++
-    }
+    if (haystack[i] === needle) count++
   }
 
   return count
 }
+
+
 
 function camelCase(sentence) {
   const arr = sentence.split(' ')
 
   for (let i = 0; i < arr.length; i++) {
     arr[i] = arr[i].toLowerCase()
-
     const letters = arr[i].split('')
 
     letters[0] = letters[0].toUpperCase()
 
     arr[i] = letters.join('')
+    arr[0] = arr[0].toLowerCase()
   }
 
   return arr.join('')
